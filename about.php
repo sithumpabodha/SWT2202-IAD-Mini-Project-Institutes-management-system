a71d4a48ba58c29a79d4e5bf577d248d39ada0b1
Sithum Pabodha <121005113+sithumpabodha@users.noreply.github.com>
2024-02-29 10:07:35 +0530

    Add files via upload

---
+++ about.php
@@ -0,0 +1,71 @@
+<?php
+session_start();
+error_reporting(0);
+include('includes/dbconnection.php');
+?>
+<!doctype html>
+<html>
+<head>
+<title>Dip Educations || About Us Page</title>
+<script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false); function hideURLbar(){ window.scrollTo(0,1); } </script>
+<!--bootstrap-->
+<link href="css/bootstrap.css" rel="stylesheet" type="text/css" media="all">
+<!--coustom css-->
+<link href="css/style.css" rel="stylesheet" type="text/css"/>
+<!--script-->
+<script src="js/jquery-1.11.0.min.js"></script>
+<!-- js -->
+<script src="js/bootstrap.js"></script>
+<!-- /js -->
+<!--fonts-->
+<link href='//fonts.googleapis.com/css?family=Open+Sans:300,300italic,400italic,400,600,600italic,700,700italic,800,800italic' rel='stylesheet' type='text/css'>
+<!--/fonts-->
+<script type="text/javascript" src="js/move-top.js"></script>
+<script type="text/javascript" src="js/easing.js"></script>
+<!--script-->
+<script type="text/javascript">
+			jQuery(document).ready(function($) {
+				$(".scroll").click(function(event){		
+					event.preventDefault();
+					$('html,body').animate({scrollTop:$(this.hash).offset().top},900);
+				});
+			});
+</script>
+<!--/script-->
+</head>
+	<body>
+<!--header-->
+<?php include_once('includes/header.php');?>
+<!-- Top Navigation -->
+<div class="banner banner5">
+	<div class="container">
+	<h2>About</h2>
+	</div>
+</div>
+<!--header-->
+<!-- About -->
+<div class="about">
+	 <div class="container">
+		 <div class="about-info-grids">
+			 <div class="col-md-5 abt-pic">
+				 <img src="images/abt.jpg" class="img-responsive" alt=""/>
+			 </div>
+			 <div class="col-md-7 abt-info-pic">
+			 <div style="text-align: start;"><font color="#7b8898" <span style="font-size: 26px;">
+			 Dip Education is a leading provider of postgraduate education programmes in various Commonwealth countries, 
+			 including Sri Lanka, Australia, New Zealand, and the United Kingdom. 
+			 Our courses are designed to equip you with the skills and knowledge to teach in diverse settings, from schools to workplaces,
+			  and to advance your career in education. Whether you want to become a primary or secondary teacher, an early childhood educator, 
+			  a vocational trainer, or an educational leader, we have a course that suits your needs and interests. Dip Education is committed 
+			  to excellence in teaching and learning, 
+			  and to fostering a community of passionate educators who make a difference in the world. Join us today and discover the benefits of a Dip 
+			  Education qualification</span></font><br>
+			</div>
+			</div>
+</div>
+<
+<!-- /About -->
+<?php include_once('includes/footer.php');?>
+<!--/copy-rights-->
+	</body>
+</html>
